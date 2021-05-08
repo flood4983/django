7533cc2d6cadb44e2cddcc0f1e7619e8937e0406
flood4983 <makszinnatullin10@gmail.com>
2021-05-08 19:31:30 +0500

    update

--- prilozhenie/views.py
+++ prilozhenie/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
 def index(request):
-    return HttpResponse("Привет! Это ответ на твой запрос:)")
+    return render(request, "prilozhenie/index.html", {"quest_list": Question.objects})
+
+def detail(request, question_id):
+    return HttpResponse("Ты ищешь вопрос номер %s." % question_id)
+
+def results(request, question_id):
+    response = "Ты ищешь результаты вопроса номер %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Вы проголосовали за вопрос номер %s" % question_id)
